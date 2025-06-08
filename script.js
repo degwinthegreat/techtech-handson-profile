6c111b2fdf4daad56247d38aaf6a0fecbee0e239
degwinthegreat <the.colledge.dropout@gmail.com>
2025-06-08 13:01:29 +0900

    ヒーロセクションを実装１

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// h1要素を取得
-const h1Element = document.querySelector('h1');
+// // h1要素を取得
+// const h1Element = document.querySelector('h1');
 
-// h1要素にクリックイベントを追加
-h1Element.addEventListener('click', () => {
-  h1Element.style.color =
-    h1Element.style.color === 'blue' ? 'red' : 'blue';
-});
+// // h1要素にクリックイベントを追加
+// h1Element.addEventListener('click', () => {
+//   h1Element.style.color =
+//     h1Element.style.color === 'blue' ? 'red' : 'blue';
+// });
